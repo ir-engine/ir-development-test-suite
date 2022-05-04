780f1a195ebcfe09a15c6f4b2612729e1e95ac53
rondoor <rondoor124@gmail.com>
2022-05-04 14:22:06 -0500

    add template

---
+++ examples/template.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react'
+
+import { LoadingCircle } from '@xrengine/client-core/src/components/LoadingCircle'
+import { LoadEngineWithScene } from '@xrengine/client-core/src/components/World/LoadEngineWithScene'
+import OfflineLocation from '@xrengine/client-core/src/components/World/OfflineLocation'
+import { LocationAction } from '@xrengine/client-core/src/social/services/LocationService'
+import { useDispatch } from '@xrengine/client-core/src/store'
+import { useEngineState } from '@xrengine/engine/src/ecs/classes/EngineService'
+
+import Layout from '@xrengine/client-core/src/components/Layout'
+import { loadSceneJsonOffline } from '@xrengine/client/src/pages/offline/utils'
+
+export default function AvatarBenchmarking () {
+  const dispatch = useDispatch()
+  const engineState = useEngineState()
+
+  const projectName = 'default-project'
+  const sceneName = 'default'
+
+  useEffect(() => {
+    dispatch(LocationAction.setLocationName(`${projectName}/${sceneName}`))
+    loadSceneJsonOffline(projectName, sceneName)
+  }, [])
+
+  return (
+    <Layout  useLoadingScreenOpacity pageTitle={'template'}>
+      {engineState.isEngineInitialized.value ? <></> : <LoadingCircle />}
+      <LoadEngineWithScene />
+      <OfflineLocation />
+    </ Layout>
+  )
+}
