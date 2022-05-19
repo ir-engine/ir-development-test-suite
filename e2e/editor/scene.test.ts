8dac7eb13659331dc474ecab0a2ac625ad6dae78
IRANKUNDA Fabrice <irankundafabrice8@gmail.com>
2022-05-19 15:55:58 +0200

    added test for loading and unloading scene

---
+++ e2e/editor/scene.test.ts
@@ -0,0 +1,28 @@
+import assert from 'assert'
+import { XREngineBot } from 'XREngine-bot/src/bot'
+
+import { BotHooks } from '@xrengine/engine/src/bot/enums/BotHooks'
+
+const domain = process.env.APP_HOST || 'https://localhost:3000'
+const editorUrl = `${domain}/editor`
+
+describe('Editor Scene Tests', () => {
+  const bot = new XREngineBot({ name: 'bot', verbose: true, headless: false })
+  before(async () => {
+    await bot.launchBrowser()
+  })
+
+  after(async () => {
+    await bot.quit()
+  })
+
+  it('should load scene', async () => {
+    await bot.enterEditor(editorUrl)
+    assert(await bot.runHook(BotHooks.SceneLoaded))
+  })
+
+  it('should unload scene', async () => {
+    await bot.runHook(BotHooks.UnLoadScene)
+    assert(!(await bot.runHook(BotHooks.SceneLoaded)))
+  })
+})
