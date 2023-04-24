f081dd7212dd79cdd4d62b07a704b7e427e7a1cb
HexaField <joshfield999@gmail.com>
2023-04-24 14:14:39 +1000

    improve offline/online scene/location hooks

--- examples/template.tsx
+++ examples/template.tsx
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react'
 import { LoadingCircle } from '@etherealengine/client-core/src/components/LoadingCircle'
 import { EngineState } from '@etherealengine/engine/src/ecs/classes/EngineState'
 import { useOfflineScene, useLoadEngineWithScene } from '@etherealengine/client-core/src/components/World/EngineHooks'
-import { useLoadLocationScene } from '@etherealengine/client-core/src/components/World/LoadLocationScene'
+import { useLoadLocationScene, useLoadScene } from '@etherealengine/client-core/src/components/World/LoadLocationScene'
 import { useDefaultLocationSystems } from '@etherealengine/client-core/src/world/useDefaultLocationSystems'
 import { getMutableState, useHookstate } from '@etherealengine/hyperflux'
 import { AvatarService } from '@etherealengine/client-core/src/user/services/AvatarService'
@@ -14,7 +14,8 @@ export function Template() {
   const projectName = 'default-project'
   const sceneName = 'default'
 
-  useOfflineScene({ projectName, sceneName, spectate: true })
+  useLoadScene({ projectName, sceneName })
+  useOfflineScene({ spectate: true })
   useLoadLocationScene()
   useLoadEngineWithScene({ spectate: true })
   useDefaultLocationSystems(true)
