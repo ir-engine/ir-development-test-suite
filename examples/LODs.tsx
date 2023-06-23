d9363f6e48ae6864be70ed26afdab141c2a519ac
HexaField <joshfield999@gmail.com>
2023-06-23 11:15:04 +1000

    update lods example to new variant component

--- examples/LODs.tsx
+++ examples/LODs.tsx
@@ -11,8 +11,10 @@ import { TransformComponent } from '@etherealengine/engine/src/transform/compone
 import { Vector3 } from 'three'
 import { VisibleComponent } from '@etherealengine/engine/src/scene/components/VisibleComponent'
 import { NameComponent } from '@etherealengine/engine/src/scene/components/NameComponent'
-import { LODComponent } from '@etherealengine/engine/src/scene/components/LODComponent'
+import { VariantComponent } from '@etherealengine/engine/src/scene/components/VariantComponent'
 import { ModelComponent } from '@etherealengine/engine/src/scene/components/ModelComponent'
+import { AdminAssetUploadArgumentsType } from '@etherealengine/common/src/interfaces/UploadAssetInterface'
+import { StaticResourceInterface } from '@etherealengine/common/src/interfaces/StaticResourceInterface'
 
 const LODsDND = () => {
   const filenames = useHookstate<string[]>([])
@@ -32,12 +34,12 @@ const LODsDND = () => {
           const uploadPromise = uploadToFeathersService('upload-asset', files, {
             type: 'admin-file-upload',
             args: {
-              staticResourceType: 'model3d',
-            },
+              project: 'ee-development-test-suite',
+            } as AdminAssetUploadArgumentsType,
             variants: true
           })
 
-          const result = await uploadPromise.promise as string[]
+          const result = await uploadPromise.promise as StaticResourceInterface[]
 
           console.log(result)
 
@@ -45,20 +47,22 @@ const LODsDND = () => {
           setComponent(entity, TransformComponent, { position: new Vector3(0, 0, -2) })
           setComponent(entity, VisibleComponent)
           setComponent(entity, NameComponent, 'LOD Test')
-          
+
           setComponent(entity, ModelComponent, {
-            src: result[0],
+            src: result[0].url,
           })
-          setComponent(entity, LODComponent, {
-            target: entity,
+          setComponent(entity, VariantComponent, {
             levels: result.map((variant, i) => ({
               distance: (i + 1) * 5,
               loaded: false,
-              src: variant,
+              src: variant.url,
               model: null,
-              metadata: {}//variant.metadata
+              metadata: variant.metadata ?? {
+                minDistance: 0 + (i * 5),
+                maxDistance: 0 + ((i + 1) * 5),
+              }
             })),
-            lodHeuristic: 'DISTANCE'
+            heuristic: 'DISTANCE'
           })
 
         } catch (err) {
