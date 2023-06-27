e1022c7276f9e3315b176abbaadd30cdd9347f87
sybiote <ghoshr698@gmail.com>
2023-06-27 18:37:42 +0530

    styling update

--- examplesRoute.tsx
+++ examplesRoute.tsx
@@ -9,12 +9,36 @@ const buttonStyle = {
   padding: '8px',
   margin: '10px',
   borderStyle: 'solid',
-  background: 'none',
+  background: '#969696',
   cursor: 'pointer',
-  outline: 'none'
-}
+  boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.3)', // Adds a slight 3D effect with a box-shadow
+  wordWrap: 'break-word',
+  borderColor: 'rgba(31, 27, 72, 0.85)', // Sets the outline color to rgb(31, 27, 72, 0.85)
 
+}
+const Navbar = () => {
+  const navbarStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '60px',
+    backgroundColor: 'rgb(31 27 72 / 85%)',
+    color: '#e7e7e7',
+  };
+
+  const headingStyle = {
+    fontSize: '1.5rem',
+    fontWeight: 'bold',
+  };
+
+  return (
+    <div style={navbarStyle}>
+      <h1 style={headingStyle}>Examples</h1>
+    </div>
+  );
+};
 //@ts-ignore
+
 const routes = import.meta.glob('./examples/*.tsx', { eager: true }) as Record<string, { default: () => JSX.Element }>
 
 const RoutesList = () => {
@@ -24,14 +48,15 @@ const RoutesList = () => {
   };
 
   return (
-    <div style={{ pointerEvents: 'all', overflow: 'auto', maxHeight: '100vh' }}>
-      <h1><center>Examples</center></h1>
+    <div style={{ pointerEvents: 'all', overflow: 'auto', height: '100vh' , background: '#02022d' }}>
+       <Navbar/>
       <div
         style={{
           display: 'grid',
           gridTemplateColumns: 'repeat(4, minmax(200px, 1fr))',
           gap: '10px',
           padding: '10px',
+
         }}
       >
         {Object.entries(routes).map(([route, { default: Element }]) => {
