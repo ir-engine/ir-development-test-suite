8f8508d3be85d49cc1eb27ec5a1d327ba2589b08
HexaField <joshfield999@gmail.com>
2023-07-19 14:38:23 +1000

    rename client hooks

--- examples/utils/template.tsx
+++ examples/utils/template.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 
 import { LoadingCircle } from '@etherealengine/client-core/src/components/LoadingCircle'
-import { useLoadEngineWithScene, useOfflineScene } from '@etherealengine/client-core/src/components/World/EngineHooks'
+import { useLoadEngineWithScene, useOfflineNetwork } from '@etherealengine/client-core/src/components/World/EngineHooks'
 import { useLoadLocationScene, useLoadScene } from '@etherealengine/client-core/src/components/World/LoadLocationScene'
 import { AvatarService } from '@etherealengine/client-core/src/user/services/AvatarService'
 import { useDefaultLocationSystems } from '@etherealengine/client-core/src/world/useDefaultLocationSystems'
@@ -12,7 +12,7 @@ export function Template(props: { projectName?: string; sceneName?: string }) {
   const engineState = useHookstate(getMutableState(EngineState))
 
   useLoadScene({ projectName: props.projectName ?? 'default-project', sceneName: props.sceneName ?? 'default' })
-  useOfflineScene({ spectate: true })
+  useOfflineNetwork({ spectate: true })
   useLoadLocationScene()
   useLoadEngineWithScene({ spectate: true })
   useDefaultLocationSystems(true)
