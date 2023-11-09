40608ad601d5f30ad3490c9b19fe073feaa14f88
MoizAdnan <moizadnan21@gmail.com>
2023-11-10 00:44:02 +0500

    Added project event hook

--- projectEventHooks.ts
+++ projectEventHooks.ts
@@ -5,8 +5,10 @@ import { Application } from '@etherealengine/server-core/declarations'
 import packageJson from './package.json'
 
 const config = {
-  onInstall: async (app: Application) => {
+  onUpdate: async (app: Application) => {
     await createLocations(app, packageJson.name)
+  },
+  onInstall: async (app: Application) => {
     await app.service('route-activate').create({ project: packageJson.name, route: '/examples', activate: true })
   }
 } as ProjectEventHooks
