a38639a1fd0d16de9561029d70a4070dc4d9f091
Ashish <ashish@aesthisia.com>
2024-01-02 12:16:53 +0530

    bot-test

--- e2e/dev/alldirection.test.ts
+++ e2e/dev/alldirection.test.ts
@@ -6,7 +6,7 @@ import { BotHooks } from 'ee-bot/src/enums/BotHooks'
 //const vector3 = new Vector3()
 
 //const domain = process.env.APP_HOST
-const domain = 'localhost'
+const domain = 'localhost:3000'
 const locationName = 'apartment'
 const sqrt2 = Math.sqrt(2)
 
