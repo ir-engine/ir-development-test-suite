4a5a0ddf75ade82bbf1c30ee5565eaaf53de812d
HexaField <joshfield999@gmail.com>
2024-01-06 12:11:06 +1100

    rig component refactor

--- examples/utils/avatar/loadAvatarHelpers.ts
+++ examples/utils/avatar/loadAvatarHelpers.ts
@@ -160,10 +160,9 @@ export const loadAssetWithLoopAnimation = async (filename, position: Vector3, i:
   })
   setComponent(entity, VisibleComponent, true)
   setComponent(entity, LoopAnimationComponent, {
-    hasAvatarAnimations: true,
     activeClipIndex: 0,
     animationPack: config.client.fileServer + '/projects/default-project/assets/animations/emotes/wave.fbx'
   })
-  setComponent(entity, ModelComponent, { src: filename, generateBVH: false, avoidCameraOcclusion: true })
+  setComponent(entity, ModelComponent, { src: filename, convertToVRM: true, cameraOcclusion: false })
   return entity
 }
