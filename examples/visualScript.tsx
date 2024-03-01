9a97316f5a37bce9d1110c7138fcce79687d45ef
sybiote <ghoshr698@gmail.com>
2024-03-01 17:01:44 +0530

    fix visual script test

--- examples/visualScript.tsx
+++ examples/visualScript.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-import { GraphJSON } from '@behave-graph/core'
+import { GraphJSON } from '@etherealengine/visual-script'
 import { ActiveVisualScript } from '@etherealengine/editor/src/components/visualScript/VisualFlow'
-import '@etherealengine/editor/src/components/graph/ee-flow/styles.css'
-import { VisualScriptComponent } from '@etherealengine/engine/src/visual-script/components/VisualScriptComponent'
+import '@etherealengine/editor/src/components/visualScript/ReactFlowStyle.css'
 import { setComponent } from '@etherealengine/ecs/src/ComponentFunctions'
 import { createEntity } from '@etherealengine/ecs/src/EntityFunctions'
 import AutoSizer from 'react-virtualized-auto-sizer'
 import 'reactflow/dist/style.css'
 import targetJson from '../assets/graph/simpleController.json'
 import { Template } from './utils/template'
+import { VisualScriptComponent } from '@etherealengine/spatial'
 
 const entity = createEntity()
 setComponent(entity, VisualScriptComponent, { visualScript: targetJson as unknown as GraphJSON })
