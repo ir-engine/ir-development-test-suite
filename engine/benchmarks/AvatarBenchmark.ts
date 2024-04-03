c29a5ee6da22f514cca400701e9baf340685b4bc
Michael Estes <michael@etherealengine.com>
2024-04-03 12:23:59 -0700

    Randomized IK targets for IK benchmark

--- engine/benchmarks/AvatarBenchmark.ts
+++ engine/benchmarks/AvatarBenchmark.ts
@@ -1,5 +1,5 @@
 import config from '@etherealengine/common/src/config'
-import { UserID, avatarPath } from '@etherealengine/common/src/schema.type.module'
+import { AvatarID, UserID, avatarPath } from '@etherealengine/common/src/schema.type.module'
 import {
   Engine,
   Entity,
@@ -20,8 +20,10 @@ import {
 } from '@etherealengine/engine/src/avatar/components/AvatarAnimationComponent'
 import { AvatarComponent } from '@etherealengine/engine/src/avatar/components/AvatarComponent'
 import { AvatarColliderComponent } from '@etherealengine/engine/src/avatar/components/AvatarControllerComponent'
+import { AvatarIKTargetComponent } from '@etherealengine/engine/src/avatar/components/AvatarIKComponents'
 import { LoopAnimationComponent } from '@etherealengine/engine/src/avatar/components/LoopAnimationComponent'
 import { AvatarIKTargetState } from '@etherealengine/engine/src/avatar/state/AvatarIKTargetState'
+import { AvatarState } from '@etherealengine/engine/src/avatar/state/AvatarNetworkState'
 import { SceneState } from '@etherealengine/engine/src/scene/Scene'
 import { ModelComponent } from '@etherealengine/engine/src/scene/components/ModelComponent'
 import { getMutableState } from '@etherealengine/hyperflux'
@@ -32,7 +34,7 @@ import { Object3DComponent } from '@etherealengine/spatial/src/renderer/componen
 import { VisibleComponent } from '@etherealengine/spatial/src/renderer/components/VisibleComponent'
 import { EntityTreeComponent } from '@etherealengine/spatial/src/transform/components/EntityTree'
 import { useEffect } from 'react'
-import { Group, MathUtils, Vector3 } from 'three'
+import { Group, MathUtils, Quaternion, Vector3 } from 'three'
 import { getSceneID, sleep, waitForPropertyLoad } from './BenchmarkUtils'
 
 const avatarsToCreate = 10
@@ -99,14 +101,44 @@ export const AvatarBenchmark = (props: { onComplete: () => void }): null => {
   return null
 }
 
-const createIkTargetsForAvatar = (userID: string) => {
+const spawnAvatar = (
+  userID: string,
+  avatarID: string,
+  pose: {
+    position: Vector3
+    rotation: Quaternion
+  }
+): Entity => {
+  const entityUUID = (userID + '_avatar') as EntityUUID
+  const avatarState = getMutableState(AvatarState)
+  const entity = UUIDComponent.getOrCreateEntityByUUID(entityUUID)
+  avatarState.merge({ [entityUUID]: { avatarID: avatarID as AvatarID, name: userID } })
+  return entity
+}
+
+const randomVec3 = (): Vector3 => {
+  return new Vector3(MathUtils.randFloat(-2.0, 2.0), MathUtils.randFloat(2.0, 4.0), MathUtils.randFloat(-2, 2.0))
+}
+
+const randomQuaternion = (): Quaternion => {
+  return new Quaternion().random()
+}
+
+const createIkTargetsForAvatar = (userID: string): Entity[] => {
   const headUUID = (userID + ikTargets.head) as EntityUUID
   const leftHandUUID = (userID + ikTargets.leftHand) as EntityUUID
   const rightHandUUID = (userID + ikTargets.rightHand) as EntityUUID
   const leftFootUUID = (userID + ikTargets.leftFoot) as EntityUUID
   const rightFootUUID = (userID + ikTargets.rightFoot) as EntityUUID
 
-  const avatarIKTargetState = getMutableState(AvatarIKTargetState) //[action.entityUUID].merge({ name: action.name })
+  const entities = [] as Entity[]
+  ;[headUUID, leftHandUUID, rightHandUUID, leftFootUUID, rightFootUUID].forEach((uuid) => {
+    const entity = UUIDComponent.getOrCreateEntityByUUID(uuid)
+    setComponent(entity, TransformComponent, { position: randomVec3(), rotation: randomQuaternion() })
+    entities.push(entity)
+  })
+
+  const avatarIKTargetState = getMutableState(AvatarIKTargetState)
 
   avatarIKTargetState.merge({
     [headUUID]: { name: 'head' },
@@ -115,10 +147,14 @@ const createIkTargetsForAvatar = (userID: string) => {
     [leftFootUUID]: { name: 'leftFoot' },
     [rightFootUUID]: { name: 'rightFoot' }
   })
+
+  return entities
 }
 
 const runAvatarIKBenchmark = async (onComplete: () => void) => {
   const entities = [] as Entity[]
+  let avatarEntity = undefined as Entity | undefined
+  let targetEntities = undefined as Entity[] | undefined
   const avatars = await Engine.instance.api.service(avatarPath).find({})
   const sceneID = getSceneID()
   const rootEntity = SceneState.getRootEntity(sceneID)
@@ -128,7 +164,9 @@ const runAvatarIKBenchmark = async (onComplete: () => void) => {
     position.setX(position.x + MathUtils.randFloat(-2.0, 2.0))
     position.setY(0)
     position.setZ(position.z - 3.0 * i - avatarsToCreate / 3)
-    const avatarSrc = avatars.data[i % avatars.data.length].modelResource?.url
+    const avatar = avatars.data[i % avatars.data.length]
+    const avatarID = avatar.id
+    const avatarSrc = avatar.modelResource?.url!
     const entity = createEntity()
     entities.push(entity)
 
@@ -151,14 +189,27 @@ const runAvatarIKBenchmark = async (onComplete: () => void) => {
       locomotion: new Vector3()
     })
     setComponent(entity, AvatarRigComponent)
-    createIkTargetsForAvatar(uuid)
+    avatarEntity = spawnAvatar(uuid, avatarID, { position, rotation: new Quaternion() })
+    targetEntities = createIkTargetsForAvatar(uuid)
     await waitForModelLoad(entity)
+
+    for (const entity of targetEntities) {
+      AvatarIKTargetComponent.blendWeight[entity] = 1
+    }
   }
 
   await sleep(benchmarkWaitTime)
+
   for (const entity of entities) {
     removeEntity(entity)
   }
+  if (avatarEntity) removeEntity(avatarEntity)
+  if (targetEntities) {
+    for (const entity of targetEntities) {
+      removeEntity(entity)
+    }
+  }
+
   await sleep(teardownWaitTime)
   onComplete()
 }
