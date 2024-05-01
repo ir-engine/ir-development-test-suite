a5048a7e0d7dbf44bf1ddcc9ca7b8b3fd695436a
Michael Estes <michael@etherealengine.com>
2024-04-30 18:45:10 -0700

    Example selector

--- engine/examples/ExampleSelectorUI.tsx
+++ engine/examples/ExampleSelectorUI.tsx
@@ -1,19 +1,56 @@
+// @ts-ignore
+import base from '@etherealengine/client/src/themes/base.css?inline'
+// @ts-ignore
+import styles from './ExampleSelectorUI.css?inline'
+
 import { useComponent, useEntityContext } from '@etherealengine/ecs'
-import React from 'react'
-import { ExamplesComponent } from './ExamplesComponent'
+import React, { useEffect, useRef } from 'react'
+import { ExamplesComponent, examples } from './ExamplesComponent'
 
 const ExampleSelectorUI: React.FC = () => {
   const entity = useEntityContext()
-  const examples = useComponent(entity, ExamplesComponent)
-  const selectedIndex = examples.currExampleIndex.value
+  const examplesComponent = useComponent(entity, ExamplesComponent)
+  const selectedIndex = examplesComponent.currExampleIndex.value
+
+  const frame = useRef(null)
+
+  const addScroll = (y) => {
+    ;(frame as any).current.scrollBy(0, y)
+  }
+
+  useEffect(() => {
+    window.addEventListener(
+      'wheel',
+      (event) => {
+        event.preventDefault()
+        addScroll(event.deltaY)
+      },
+      { passive: false, capture: true }
+    )
+  }, [])
 
   return (
     <>
-      <div>
-        <div>
-          <h2 style={{ color: 'white' }}>Test Header</h2>
-          <p>Test Body</p>
-        </div>
+      <style type="text/css">{base.toString()}</style>
+      <style type="text/css">{styles.toString()}</style>
+      <div className="ExamplesContainer" ref={frame}>
+        {examples.map((example, index) => {
+          return (
+            <div
+              className={index === selectedIndex ? 'SelectedExampleContainer' : 'ExampleContainer'}
+              key={example.name}
+              onPointerDown={() => {
+                examplesComponent.currExampleIndex.set(index)
+                console.log(`Clicked index: ${index}`)
+              }}
+            >
+              <div className="ExampleTextContainer">
+                <h2 className="ExampleName">{example.name}</h2>
+                <p className="ExampleDescription">{example.description}</p>
+              </div>
+            </div>
+          )
+        })}
       </div>
     </>
   )
--- engine/examples/ExamplesComponent.tsx
+++ engine/examples/ExamplesComponent.tsx
@@ -1,8 +1,9 @@
-import {
+import ECS, {
   Entity,
   UndefinedEntity,
   createEntity,
   defineComponent,
+  removeEntity,
   useComponent,
   useEntityContext
 } from '@etherealengine/ecs'
@@ -22,10 +23,70 @@ export const examples: Example[] = [
     name: 'Models',
     description: 'Add GLTF models to your scene',
     setup: (entity: Entity) => {
-      console.log('Creating Model Component Example')
+      console.log('Creating Model Example')
     },
     teardown: (entity: Entity) => {
-      console.log('Tearing down Model Component Example')
+      console.log('Tearing down Model Example')
+    }
+  },
+  {
+    name: 'Avatars',
+    description: 'Add avatars to your scene',
+    setup: (entity: Entity) => {
+      console.log('Creating Avatar Example')
+    },
+    teardown: (entity: Entity) => {
+      console.log('Tearing down Avatar Example')
+    }
+  },
+  {
+    name: 'Variants',
+    description: 'Load multiple variants of a model',
+    setup: (entity: Entity) => {
+      console.log('Creating Variant Example')
+    },
+    teardown: (entity: Entity) => {
+      console.log('Tearing down Variant Example')
+    }
+  },
+  {
+    name: 'Particles',
+    description: 'Add particle systems to your scene',
+    setup: (entity: Entity) => {
+      console.log('Creating Particle Example')
+    },
+    teardown: (entity: Entity) => {
+      console.log('Tearing down Particle Example')
+    }
+  },
+  {
+    name: 'Links',
+    description: 'Add links to your scene',
+    setup: (entity: Entity) => {
+      console.log('Creating Link Example')
+    },
+    teardown: (entity: Entity) => {
+      console.log('Tearing down Link Example')
+    }
+  },
+  {
+    name: 'Splines',
+    description: 'Add splines to your scene',
+    setup: (entity: Entity) => {
+      console.log('Creating Spline Example')
+    },
+    teardown: (entity: Entity) => {
+      console.log('Tearing down Spline Example')
+    }
+  },
+  {
+    name: 'Animations',
+    description: 'Add animated models to your scene',
+    setup: (entity: Entity) => {
+      console.log('Creating Animation Example')
+    },
+    teardown: (entity: Entity) => {
+      console.log('Tearing down Animation Example')
     }
   }
 ]
@@ -52,18 +113,30 @@ export const ExamplesComponent = defineComponent({
     const examplesComponent = useComponent(entity, ExamplesComponent)
 
     useEffect(() => {
-      const ui = createXRUI(ExampleSelectorUI, null, { interactable: true }, entity)
+      const selectExampleUI = createXRUI(ExampleSelectorUI, null, { interactable: true }, entity)
+      selectExampleUI.container.position.set(-2, 1.5, -1)
     }, [])
 
     useEffect(() => {
       const example = examples[examplesComponent.currExampleIndex.value]
-      const entity = createEntity()
-      example.setup(entity)
+      const exampleEntity = createEntity()
+      example.setup(exampleEntity)
+      examplesComponent.currExample.set(exampleEntity)
 
       return () => {
-        if (example.teardown) example.teardown(entity)
+        if (example.teardown) example.teardown(exampleEntity)
+        removeEntity(exampleEntity)
       }
     }, [examplesComponent.currExampleIndex])
+
+    useEffect(() => {
+      const currExample = examplesComponent.currExample.value
+      if (!currExample) return
+
+      const components = ECS.getAllComponents(currExample)
+
+      return () => {}
+    }, [examplesComponent.currExample])
 
     return <></>
   }
