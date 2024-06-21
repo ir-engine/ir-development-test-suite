e60e3d7961c14d5cabf8b470a652d9227f16b8a9
Michael Estes <michael@etherealengine.com>
2024-06-20 18:16:16 -0700

    Profiling helper

--- src/benchmarks/heapBenchmark.tsx
+++ src/benchmarks/heapBenchmark.tsx
@@ -57,7 +57,7 @@ export default function HeapBenchmarkEntry() {
   return sceneEntity && memory ? (
     <>
       <ProfilerUI systemUUIDs={[]} />
-      <div style={{ position: 'absolute', right: 12, top: 36, textAlign: 'right', pointerEvents: 'auto' }}>
+      <div style={{ position: 'absolute', right: 12, bottom: 36, textAlign: 'right', pointerEvents: 'auto' }}>
         <div>{`Used Heap: ${Math.trunc(memory.usedJSHeapSize * 0.000001)}mb`}</div>
         <button style={buttonStyle} onClick={() => setGrow(!grow)}>
           {grow ? 'Stop' : 'Grow'}
