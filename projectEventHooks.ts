0039316ff33f19d8e46a3b671ac1b2d4ae05bd05
HexaField <joshfield999@gmail.com>
2024-08-17 13:01:41 +1000

    update project name

--- projectEventHooks.ts
+++ projectEventHooks.ts
@@ -8,16 +8,15 @@ import appRootPath from 'app-root-path'
 import fs from 'fs'
 import path from 'path'
 import manifestJson from './manifest.json'
-import packageJson from './package.json'
 
 const projectRelativeFolder = path.resolve(appRootPath.path, 'packages/projects')
-const avatarsFolder = path.join(appRootPath.path, 'packages/projects/projects', packageJson.name, 'public/avatars')
+const avatarsFolder = path.join(appRootPath.path, 'packages/projects/projects', manifestJson.name, 'public/avatars')
 
 const config = {
   onInstall: async (app: Application) => {
-    await app.service('route-activate').create({ project: packageJson.name, route: '/examples', activate: true })
-    await app.service('route-activate').create({ project: packageJson.name, route: '/benchmarks', activate: true })
-    await app.service('route-activate').create({ project: packageJson.name, route: '/benchmarksAll', activate: true })
+    await app.service('route-activate').create({ project: manifestJson.name, route: '/examples', activate: true })
+    await app.service('route-activate').create({ project: manifestJson.name, route: '/benchmarks', activate: true })
+    await app.service('route-activate').create({ project: manifestJson.name, route: '/benchmarksAll', activate: true })
 
     await Promise.all(
       fs
